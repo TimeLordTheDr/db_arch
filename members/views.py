f32552fd506c67ba2b793bb875470a6f6b35a023
Pav Ly <repositoryuniversity@gmail.com>
2024-08-20 23:09:33 +0300

    added details page. details ctrl urls+=all/details/<int:id>

--- members/views.py
+++ members/views.py
@@ -20,8 +20,19 @@ from django.template import loader
 
 def all(request):
   mymembers = Member.objects.all().values()
-  template = loader.get_template('all_members.html')
+  # template = loader.get_template('all_members.html')
   context = {
     'mymembers': mymembers,
   }
-  return HttpResponse(template.render(context, request))
+  # return HttpResponse(template.render(context, request))
+  return render(request, 'all_members.html', context)
+
+
+def details(request, id):
+  mymember = Member.objects.get(id=id)
+  # template = loader.get_template('details.html')
+  context = {
+    'mymember': mymember,
+  }
+  # return HttpResponse(template.render(context, request))
+  return render(request, 'details.html', context)
