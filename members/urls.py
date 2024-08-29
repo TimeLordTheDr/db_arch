1d16401aea277f0dc8163229a90e797f7636c941
Pav Ly <repositoryuniversity@gmail.com>
2024-08-29 12:33:49 +0300

    my_first.html::urls and  all_members.html::a href="{% url details x.id%}  play around

--- members/urls.py
+++ members/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('members/', views.members, name='members'),
+    path('my_first_html/', views.my_first_html, name='my_first_html'),
     path('all/', views.all, name='all'),
     path('all/details/<int:id>', views.details, name='details'),
 ]
--- members/views.py
+++ members/views.py
@@ -6,13 +6,19 @@ from django.http import HttpResponse
 
 from .models import Member
 
-def members(request):
+def my_first_html(request):
     # return HttpResponse("Hello world!")
     # template = loader.get_template('myfirst.html')
     # return HttpResponse(template.render())
    
     # return render(request, 'myfirst.html', context)
-    return render(request, 'myfirst.html',)
+    mymembers = Member.objects.all().values()
+  # template = loader.get_template('all_members.html')
+    context = {
+    'mymembers': mymembers,
+    }
+    
+    return render(request, 'myfirst.html', context)
     
 
 from django.template import loader
